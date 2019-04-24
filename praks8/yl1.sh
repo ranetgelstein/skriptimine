49cf8cbfb85bddb14d08d9d614a066426402de0e
Ranet Gelštein <ranet.gelstein@khk.ee>
2019-04-24 18:52:35 +0300

    paaris

---
+++ praks8/yl1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n "Sisesta number mile piires liidetakse paaris arvud: "; read arvu
+summa=0
+
+for ((arv=1; arv<=$arvu; arv++ ))
+do
+
+	jaak=$(($arv % 2))
+	if [ $jaak == 0 ]; then
+		summa=$(($summa + $arv))
+		echo $summa
+
+	fi
+done
+echo "Arvud bahemikus 1 kuni $arvu (kaasaarvatud paarisarvude summa on: $summa
